b63dcfba581e3181dda0275f1561b370bc714703
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2019-10-01 10:35:59 +0100

    :memo: Add some comments.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import scrollIntoView from './js/scrollTo'
 const node = document.querySelector('#elm-container')
 const flags = {}
 
+// elmPromisify resolves an elm app after the next animation frame, guaranteeing
+// the elm app gets a chance to render at least once.
 elmPromisify(Elm.Main, { node, flags }).then(app => {
   // elm -> js ports
   app.ports.scrollToElement.subscribe(scrollIntoView)
